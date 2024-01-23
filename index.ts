a0e7a8ca668e4a22721368fd12f0da134824748f
Anas0055 <manaskhan969@gmail.com>
2024-01-23 02:34:36 -0800

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+// Names: Store the names of a few of your friends in a array
+//  called names. Print each person’s name by accessing each 
+//  element in the list, one at a time.
+
+let myarr =["ali","khan","ahmad","muhammad"]
+// console.log (myarr[0])
+// console.log (myarr[1])
+// console.log (myarr[2])
+// console.log (myarr[3])
+for (let i=0; i<myarr.length; i++){
+    console.log(myarr[i])
+}
